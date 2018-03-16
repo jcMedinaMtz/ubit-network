ee78a6a9196418fff86efd6fab3dc51ed655598e
Miguel Amatón <kawatzaki@gmail.com>
2018-03-16 07:51:15 -0600

    + Agregado el guardado y todo bien

--- php/register.php
+++ php/register.php
@@ -1,4 +1,26 @@
 <?php
+    // Información de la base de datos que definitivamente no debería de ir aquí, pero bueno
+    $user = 'root';
+    $pass = '';
+    $DB = new PDO(
+        'mysql:host=localhost;dbname=unboundit', $user, $pass) or die 
+        ('Imposible conectar con la base de datos');
+        
+    $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Registrar al usuario y verificar que todo salga bien
+    try {
+        $success = register($DB);
+        if ($success) {
+            echo 'Se registro al usuario con éxito';
+        } else {
+            echo 'Ocurrió un error, quizá debas intentar de nuevo';
+        }
+
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
+
     /**
      * Función de registro principal
      * @return
@@ -29,37 +51,38 @@
         $is_network = $is_network == 'on' ? 1 : 0;
 
         // Guardar lo que se tiene hasta ahora del lead
-        $save = $DB->prepare('INSERT INTO leads (name, email, profession, is_network) VALUES (?, ?, ?, ?)');
-        $save = $DB->execute($name, $email, $profession, $is_network);
+        $leadStatement = $DB->prepare('INSERT INTO leads (name, email, profession, is_network) VALUES (?, ?, ?, ?)');
+        $save = $leadStatement->execute([$name, $email, $profession, $is_network]);
 
         // Verificar si se logró guardar el LEAD
         if ($save) {
-            $lead_id = $DB->lastInsertId;
+            $lead_id = $DB->lastInsertId();
         } else {
-            throw new Exception('No se logró guardar el LEAD');
+            throw new Exception('El correo ya está registrado, u ocurrió un error al registrar sus datos');
         }
 
         // Dependiendo del resultado anterior revisamos si guardaremos intereses o habilidaes
         $interests = $_POST['interests'] ?? null;
         if (!empty($interests) && is_string($interests)) {
-            $interests = explode(',', $interests);
-            $DB->prepare('INSERT INTO table interests (name, lead_id) values (?, ?)');
+
+            $interests = explode(',', trim($interests));
+            $interestStatement = $DB->prepare('INSERT INTO interests (description, lead_id) values (?, ?)');
 
             foreach ($interests as $interest) {
-                $DB->execute($interest, $lead_id);
+                $interestStatement->execute([$interest, $lead_id]);
             }
         }
 
         // Consultar las habilidades en caso de que se trate de un interesado por unbound it
         if ($is_network) {
-            $skills = $_POST['interests'] ?? null;
+            $skills = $_POST['skills'] ?? null;
             if (!empty($skills) && is_string($skills)) {
-                $skills = explode(',', $skills);
 
-                $DB->prepare('INSERT INTO table skills (name, lead_id) values (?, ?)');
+                $skills = explode(',', trim($skills));
+                $skillsStatement = $DB->prepare('INSERT INTO skills (description, lead_id) values (?, ?)');
             
                 foreach ($skills as $skill) {
-                    $DB->execute($skill, $lead_id);
+                    $skillsStatement->execute([$skill, $lead_id]);
                 }
             }
         }
@@ -67,5 +90,4 @@
         // Si llegamos hasta aquì todo salió bien
         return true;
     }
-
 ?>
