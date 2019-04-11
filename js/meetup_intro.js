ab0d1236ac8636a5a72409683d7dd9c9da324416
MEDINAJU <juancarlos.medinamartinez@tyson.com>
2019-04-11 12:32:45 -0500

    intro

---
+++ js/meetup_intro.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(".text").typed({
+		strings: ["DEVELOPMENT", "LEARNING", "BEER", "NETWORKING"],
+		typeSpeed: 70,
+		backSpeed: 20,
+		backDelay: 3000,
+		showCursor: true,
+		loop: true
+		});
+});
